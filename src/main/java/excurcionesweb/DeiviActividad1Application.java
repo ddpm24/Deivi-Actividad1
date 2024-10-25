92295e2690e991910a2aec8d112f143ee447bf1f
Deivi <deivi54357326@gmail.com>
2024-10-25 16:13:28 +0200

    Initial commit

---
+++ src/main/java/excurcionesweb/DeiviActividad1Application.java
@@ -0,0 +1,13 @@
+package excurcionesweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeiviActividad1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeiviActividad1Application.class, args);
+	}
+
+}
