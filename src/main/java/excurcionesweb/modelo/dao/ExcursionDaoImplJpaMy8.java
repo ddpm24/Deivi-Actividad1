084936c779eba23c6834103e78d3bdc68e9f9d3b
Deivi <deivi54357326@gmail.com>
2024-10-25 18:30:00 +0200

    Create ExcursionDao interface for excursion operations management

---
+++ src/main/java/excurcionesweb/modelo/dao/ExcursionDaoImplJpaMy8.java
@@ -0,0 +1,69 @@
+package excurcionesweb.modelo.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import excurcionesweb.modelo.entities.Excursion;
+import excurcionesweb.modelo.repository.ExcursionRepository;
+
+@Repository
+public class ExcursionDaoImplJpaMy8 implements ExcursionDao {
+
+	@Autowired
+	private ExcursionRepository erepo;
+	
+	@Override
+	public Excursion findById(int idExcursion) {
+		// TODO Auto-generated method stub
+		 return erepo.findById(Integer.valueOf(idExcursion)).orElse(null);
+	}
+
+	@Override
+	public List<Excursion> findAll() {
+		// TODO Auto-generated method stub
+		return erepo.findAll();
+	}
+
+	@Override
+	public int insertOne(Excursion excursion) {
+		// TODO Auto-generated method stub
+		return (erepo.save(excursion) != null) ? 1 : 0;
+	}
+
+	@Override
+	public int updateOne(Excursion excursion) {
+		// TODO Auto-generated method stub
+		if (erepo.existsById(excursion.getIdExcursion())) {
+			erepo.save(excursion);
+			return 1;
+		}
+		return 0;
+	}
+
+	@Override
+	public List<Excursion> findByCreados() {
+		// TODO Auto-generated method stub
+		return erepo.findByCreados();
+	}
+
+	@Override
+	public List<Excursion> findByCancelados() {
+		// TODO Auto-generated method stub
+		return erepo.findByCancelados();
+	}
+
+	@Override
+	public List<Excursion> findByDestacados() {
+		// TODO Auto-generated method stub
+		return erepo.findByDestacados();
+	}
+
+	@Override
+	public List<Excursion> findByTerminados() {
+		// TODO Auto-generated method stub
+		return erepo.findByTerminados();
+	}
+
+}
--- src/main/java/excurcionesweb/modelo/repository/ExcursionRepository.java
+++ src/main/java/excurcionesweb/modelo/repository/ExcursionRepository.java
@@ -1,6 +1,9 @@
 package excurcionesweb.modelo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import excurcionesweb.modelo.entities.Excursion;
 
@@ -8,5 +11,13 @@ import excurcionesweb.modelo.entities.Excursion;
 
 public interface ExcursionRepository extends JpaRepository<Excursion, Integer>{
 
+	@Query("select e from Excursion e where e.destacado = 'S'")
+	public List<Excursion> findByDestacados();
+	@Query("select e from Excursion e where e.estado = 'CREADO'")
+	public List<Excursion> findByCreados();
+	@Query("select e from Excursion e where e.estado = 'CREADO'")
+	public List<Excursion> findByCancelados();
+	@Query("select e from Excursion e where e.estado = 'TERMINADO'")
+	public List<Excursion> findByTerminados();
 	
 }
