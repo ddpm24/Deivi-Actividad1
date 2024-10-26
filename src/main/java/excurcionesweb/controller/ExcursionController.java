3e575facbfa26003acb54058c55358ecb759e058
Deivi <deivi54357326@gmail.com>
2024-10-26 16:51:48 +0200

    feat: add ExcursionController

---
+++ src/main/java/excurcionesweb/controller/ExcursionController.java
@@ -0,0 +1,34 @@
+package excurcionesweb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import excurcionesweb.modelo.dao.ExcursionDao;
+import excurcionesweb.modelo.entities.Excursion;
+
+@Controller
+@RequestMapping("/excursiones")
+public class ExcursionController {
+
+	@Autowired
+	private ExcursionDao edao;
+	
+	@GetMapping("/detalle/{idProducto}")
+	public String verDetalle(Model model, @PathVariable int idExcursion) {
+		
+		Excursion excursion = edao.findById(idExcursion);
+		
+		if (excursion != null) {
+			model.addAttribute("excursion", excursion);
+			return "VerDetalle";
+		} else {
+			model.addAttribute("mensaje", "excursion no existe");
+			return "forward:/";
+		}
+		
+	}
+}
